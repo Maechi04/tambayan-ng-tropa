e7f35d31c5ca17349ac6bd9187411de35dac6f9d
Maechi04 <101026136+Maechi04@users.noreply.github.com>
2025-12-25 12:28:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("loginBtn").onclick = () => {
+  alert("Login feature coming soon!");
+};
+
+document.getElementById("signupBtn").onclick = () => {
+  alert("Sign up feature coming soon!");
+};
